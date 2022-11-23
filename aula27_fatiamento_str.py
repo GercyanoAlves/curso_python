8d3d993be015858133096d5ce266a7ba46268773
GercyanoAlves <gercyanoalves@gmail.com>
2022-11-23 11:32:10 -0300

    curso logica programação

---
+++ aula27_fatiamento_str.py
@@ -0,0 +1,11 @@
+"""
+Fatiamento de str
+012345678
+Olá Mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[0:len(variavel):2])
---
+++ aula28_exercicio.py
@@ -0,0 +1,34 @@
+"""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuario para digitar sua idade
+Se o nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Se nome contém (ou não) espaços
+        Seu tem {n} letras
+        A primeira Letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade:
+    exiba "Desculpe, você deixou campos vazios"
+"""
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+if nome and idade:
+
+    print(f'Olá {nome}, você tem {idade} anos!')
+    print(f'Seu nome invertido é :{nome[::-1]}')
+
+    if ' ' in nome:
+        print("Se nome CONTÉM    espaços")
+    else:
+        print("Se nome contém NÃO espaços")
+    print(f'Seu nome contem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}.')
+    print(f'A última letra do seu nome é {nome[-1]}.')
+else:
+    print("Desculpe, você deixou campos vazios")
+
+
